8d8f3daba594c346ea4071b654e28285362a7272
fadhilijane <janefadhili@gmail.com>
2024-07-25 09:10:52 +0000

    k

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -2,10 +2,7 @@ export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
-  //disabled lint
-  if (trueOrFalse) {
-    
-  }
+  if (true0rFalse) {}
 
   return [task, task2];
 }
