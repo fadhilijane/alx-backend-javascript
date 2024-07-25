90b5537bd1eefbb60b8804682800273da3d24a59
fadhilijane <janefadhili@gmail.com>
2024-07-25 09:38:39 +0000

    fix

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,9 +1,10 @@
-export default function taskBlock(trueOrFalse) {
+export default function taskBlock(trueOrFalse) { // eslint-disable-line no-unused-vars
   const task = false;
   const task2 = true;
 
   // eslint-disable-next-line no-empty
-  if (true0rFalse) {
+  if (true0rFalse) { // eslint-disable-line no-undef
+
   }
 
   return [task, task2];
