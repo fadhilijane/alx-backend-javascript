9d0f09c335fd606b75169204288d5169ada6a0e2
fadhilijane <janefadhili@gmail.com>
2024-07-25 09:56:09 +0000

    //eslint

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -2,9 +2,8 @@ export default function taskBlock(trueOrFalse) { // eslint-disable-line no-unuse
   const task = false;
   const task2 = true;
 
-  // eslint-disable-next-line no-empty
-  if (true0rFalse) { // eslint-disable-line no-undef
-
+  if (trueOrFalse) { // eslint-disable-line no-empty
+    // eslint-disable-line no-undef
   }
 
   return [task, task2];
